726c5b82089b39add74921441782f46a12307275
AkshaySingh2005 <akshaysengar15671@google.com>
2025-10-06 19:19:30 +0530

    first deployement

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,4 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: true, // listen on all interfaces
-    allowedHosts: true, // allow external hosts (ngrok, LAN, etc.)
-  },
 });
